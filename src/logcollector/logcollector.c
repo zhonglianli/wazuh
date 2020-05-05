12c0a6273254374b02a37dd402521ebe4282b8a2
Daniel Melgarejo <danimegar@gmail.com>
2020-05-05 12:57:17 +0200

    Fixed arguments passed null for winagent

--- src/logcollector/logcollector.c
+++ src/logcollector/logcollector.c
@@ -265,7 +265,9 @@ void LogCollectorStart()
             /* On Windows we need to forward the seek for wildcard files */
 #ifdef WIN32
             set_read(current, i, j);
-            minfo(READING_FILE, current->file);
+            if (current->file) {
+                minfo(READING_FILE, current->file);
+            }
 
             if (current->fp) {
                 current->read(current, &r, 1);
